a28088de16cf95bc141601aca2720be1aab07308
Kang Minchul <tegongkang@gmail.com>
2023-12-04 16:28:36 +0900

    Create rogue_metric.py

---
+++ rogue_metric.py
@@ -0,0 +1,87 @@
+!# scipts/bin/python3
+
+'''
+ https://github.com/rickiepark/nlp-with-transformers/blob/main/06_summarization.ipynb
+ !pip install evaluate
+'''
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from datasets import load_dataset
+import evaluate
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+rouge_metric = evaluate.load("rouge")
+
+# CNN/DailyMail 데이터셋에서 PEGASUS 평가하기
+
+# "cnn_dailymail" 데이터셋 다운로드 에러가 발생할 경우 대신 "ccdv/cnn_dailymail"을 사용하세요.
+dataset = load_dataset("cnn_dailymail", version="3.0.0")
+rouge_metric = evaluate.load("rouge", cache_dir=None)
+rouge_names = ["rouge1", "rouge2", "rougeL", "rougeLsum"]
+
+def evaluate_summaries_baseline(dataset, metric,
+                                column_text="article",
+                                column_summary="highlights"):
+    summaries = [three_sentence_summary(text) for text in dataset[column_text]]
+    metric.add_batch(predictions=summaries,
+                     references=dataset[column_summary])
+    score = metric.compute()
+    return score
+
+test_sampled = dataset["test"].shuffle(seed=42).select(range(1000))
+
+score = evaluate_summaries_baseline(test_sampled, rouge_metric)
+rouge_dict = dict((rn, score[rn]) for rn in rouge_names)
+
+# 확인용 출력
+# pd.DataFrame.from_dict(rouge_dict, orient="index", columns=["baseline"]).T
+
+from tqdm import tqdm
+import torch
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+def chunks(list_of_elements, batch_size):
+    """list_of_elements로부터 batch_size 크기의 청크를 연속적으로 생성합니다"""
+    for i in range(0, len(list_of_elements), batch_size):
+        yield list_of_elements[i : i + batch_size]
+
+def evaluate_summaries_pegasus(dataset, metric, model, tokenizer,
+                               batch_size=16, device=device,
+                               column_text="article",
+                               column_summary="highlights"):
+    article_batches = list(chunks(dataset[column_text], batch_size))
+    target_batches = list(chunks(dataset[column_summary], batch_size))
+
+    for article_batch, target_batch in tqdm(
+        zip(article_batches, target_batches), total=len(article_batches)):
+
+        inputs = tokenizer(article_batch, max_length=1024,  truncation=True,
+                        padding="max_length", return_tensors="pt")
+
+        summaries = model.generate(input_ids=inputs["input_ids"].to(device),
+                         attention_mask=inputs["attention_mask"].to(device),
+                         length_penalty=0.8, num_beams=8, max_length=128)
+
+        decoded_summaries = [tokenizer.decode(s, skip_special_tokens=True,
+                                clean_up_tokenization_spaces=True)
+               for s in summaries]
+        decoded_summaries = [d.replace("<n>", " ") for d in decoded_summaries]
+        metric.add_batch(predictions=decoded_summaries, references=target_batch)
+
+    score = metric.compute()
+    return score
+
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+model_ckpt = "google/pegasus-cnn_dailymail"
+tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_ckpt).to(device)
+score = evaluate_summaries_pegasus(test_sampled, rouge_metric,
+                                   model, tokenizer, batch_size=8)
+rouge_dict = dict((rn, score[rn]) for rn in rouge_names)
+
+# 확인용 출력
+# pd.DataFrame(rouge_dict, index=["pegasus"])
